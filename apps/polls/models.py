b3f8f4cd3520d00e7f3fbccea39306e11b3731f7
Dat Phan <datphan0110@gmail.com>
2013-08-05 13:15:20 +0700

    More comprehensive tests

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
@@ -7,7 +8,8 @@ class Poll(models.Model):
         return self.question
 		
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+    	return now - datetime.timedelta(days=1) <= self.pub_date <  now
 		
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = True
